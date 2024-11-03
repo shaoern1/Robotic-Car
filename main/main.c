c30ea11e053b26e5c44077dd4b1d49c2ae760ec2
The1Azreen <2200581@sit.singaporetech.edu.sg>
2024-11-03 14:40:48 +0800

    Deleted the old branch going to start station 1

--- main/main.c
+++ main/main.c
@@ -1,58 +1,28 @@
-// Main program
-
+#include "FreeRTOS.h"
+#include "task.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include <string.h>
-#include "hardware/sync.h"
-#include "barcode.h"
-#include "motor.h" // Include motor header
 #include "pico/stdlib.h"
 
-// Function to init all sensors and motors
-void initAll()
-{
-    // Initialise standard I/O
-    stdio_init_all();
 
-    // Initialise barcode sensor pin
-    init_barcode();
-    printf("1/9 - Barcode sensor pin initialised\n");
-
-    // Initialise motor
-    init_motor();
-    printf("2/9 - Motor initialised\n");
+void led_task()
+{   
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(100);
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(100);
+    }
 }
 
 int main()
 {
-    // Init all required
-    initAll();
+    stdio_init_all();
 
-    // Variables to keep track of time
-    absolute_time_t last_barcode_check = get_absolute_time();
-    absolute_time_t last_motor_move = get_absolute_time();
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
 
-    while (1)
-    {
-        // Check barcode sensor every 100 ms
-        if (absolute_time_diff_us(last_barcode_check, get_absolute_time()) >= 100000)
-        {
-            track_bars();
-            last_barcode_check = get_absolute_time();
-        }
-
-        // Move motor forward every 100 ms
-        if (absolute_time_diff_us(last_motor_move, get_absolute_time()) >= 100000)
-        {
-            move_fwd();
-            last_motor_move = get_absolute_time();
-        }
-
-        // Allow other processes to run
-        tight_loop_contents();
-    }
-
-    return 0;
+    while(1){};
 }
